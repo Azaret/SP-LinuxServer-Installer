214fae27eeda3f5d3232edacd9bdc58d4e5647a0
Purexo <contact@purexo.eu>
2016-01-22 11:26:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 # @author : Purexo
 # https://github.com/purexo/SP-LinuxServer-Installer
-echo "You'll need NodeJS 4.x callable by node keyword, press ctrl-c if you don't have it"
-echo "Or press enter to continue"
-read
+
+function npm_update {
+  npm install
+  npm run build
+}
+
+function prerequisitory_warning {
+  echo "You'll need NodeJS 4.x callable by node keyword, press ctrl-c if you don't have it"
+  echo "Or press enter to continue"
+  read
+}
+
+function run_instructions {
+  echo 'Superpowers is ready and install with supGame, supWeb, and supLove systems'
+  echo 'run "node server start" and access it with "http://localhost:4237/" adress'
+  echo 'press ctrl-c to cancel it'
+}
+
+prerequisitory_warning
 
 git clone https://github.com/superpowers/superpowers --recursive
 cd superpowers
@@ -14,8 +30,6 @@ git clone 'https://github.com/superpowers/superpowers-game' systems/supGame --re
 git clone 'https://github.com/superpowers/superpowers-web' systems/supWeb --recursive
 git clone 'https://github.com/superpowers/superpowers-love2d' systems/supLove --recursive
 
-npm run build
+npm_update
 
-echo 'Superpowers is ready and install with supGame, supWeb, and supLove systems'
-echo 'run "node server" and access it with "http://localhost:4237/" adress'
-echo 'press ctrl-c to cancel it'
+run_instructions
